7630a2dbc1cc5f43ea24754434032d6e46800e41
Arjen Janssen <arjenjanssen@gmail.com>
2017-02-14 19:02:33 +0100

    config settings

--- _site/js/animations.js
+++
@@ -1,8 +0,0 @@
-// set equal heights to all divs in row
-$(function() {
-    $('.fadeCard').matchHeight();
-});
-// toggle class of statNav
-$(".loenzEye").on("click", function(){
-  $("#statNav").toggleClass("clicked unClicked");
-});
